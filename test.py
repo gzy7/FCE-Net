5622605df96250ef9d21a64d505d7f51c5e2870e
MathGzy7 <158469396+gzy7@users.noreply.github.com>
2025-08-08 01:05:23 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn.functional as F
+from torchmetrics.functional import structural_similarity_index_measure
+from model import LYT
+from dataloader import create_dataloaders
+import os
+from torchvision.utils import save_image
+
+def calculate_psnr(img1, img2, max_pixel_value=1.0, gt_mean=True):
+    if gt_mean:
+        img1_gray = img1.mean(axis=1)
+        img2_gray = img2.mean(axis=1)
+
+        mean_restored = img1_gray.mean()
+        mean_target = img2_gray.mean()
+        img1 = torch.clamp(img1 * (mean_target / mean_restored), 0, 1)
+
+    mse = F.mse_loss(img1, img2, reduction='mean')
+    if mse == 0:
+        return float('inf')
+    psnr = 20 * torch.log10(max_pixel_value / torch.sqrt(mse))
+    return psnr.item()
+
+def calculate_ssim(img1, img2, max_pixel_value=1.0, gt_mean=True):
+    if gt_mean:
+        img1_gray = img1.mean(axis=1, keepdim=True)
+        img2_gray = img2.mean(axis=1, keepdim=True)
+
+        mean_restored = img1_gray.mean()
+        mean_target = img2_gray.mean()
+        img1 = torch.clamp(img1 * (mean_target / mean_restored), 0, 1)
+
+    ssim_val = structural_similarity_index_measure(img1, img2, data_range=max_pixel_value)
+    return ssim_val.item()
+
+def validate(model, dataloader, device, result_dir):
+    model.eval()
+    total_psnr = 0
+    total_ssim = 0
+    with torch.no_grad():
+        for idx, (low, high) in enumerate(dataloader):
+            low, high = low.to(device), high.to(device)
+            output = model(low)
+            output = torch.clamp(output, 0, 1)
+
+            save_image(output, os.path.join(result_dir, f'result_{idx}.png'))
+
+            psnr = calculate_psnr(output, high)
+            total_psnr += psnr
+
+            ssim = calculate_ssim(output, high)
+            total_ssim += ssim
+
+    avg_psnr = total_psnr / len(dataloader)
+    avg_ssim = total_ssim / len(dataloader)
+    return avg_psnr, avg_ssim
+
+def main():
+    test_low = ''
+    test_high = ''
+    weights_path = ''
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    dataset_name = test_low.split('/')[1]
+    result_dir = os.path.join('results', dataset_name)
+    os.makedirs(result_dir, exist_ok=True)
+
+    _, test_loader = create_dataloaders(None, None, test_low, test_high, crop_size=None, batch_size=1)
+    print(f'Test loader: {len(test_loader)}')
+
+    model = LYT().to(device)
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+    print(f'Model loaded from {weights_path}')
+
+    avg_psnr, avg_ssim = validate(model, test_loader, device, result_dir)
+    print(f'Validation PSNR: {avg_psnr:.6f}, SSIM: {avg_ssim:.6f}')
+
+if __name__ == '__main__':
+    main()
